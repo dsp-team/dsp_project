e6ee73fed8b17edfa037c7c610dd218c0be67dea
gezuxia <gezuxia@163.com>
2018-03-05 13:08:19 +0800

    remove webpack-dev-server and add that

---
+++ src/pages/apply_templet/detail/templet_detail.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="templet_detail">
+    <div class="content">
+      <div>123</div>
+    </div>
+  </div>
+</template>
+<script>
+// import mapGetters from 'vuex'
+export default {
+}
+</script>
+
